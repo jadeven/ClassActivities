34753b50713fdd55966d401b072bb91781f87df2
jadeven <deveneyjoseph@gmail.com>
2022-11-04 13:02:27 -1000

    added classwork

---
+++ Section02/2.2/2.2.4JSMorePrimitives.js
@@ -0,0 +1,33 @@
+// create five variables and assign them values
+// each variable should reference a different primitive data type
+// REMEMBER: you should never DELIBERATELY assign "undefined" to a variable
+
+const catName = 'ted'
+let catAge = 2;
+let gremlinFactor = true
+let catColor = null;
+let catRelationship ;
+
+// print the type of each variable in this order:
+// string, number, boolean, undefined, null
+
+
+typeof catName;
+typeof catAge ;
+typeof gremlinFactor;
+
+// create a variable that references a template literal
+// inside the template literal, use two of the above variables
+let catInfo = `the cats name is ${catName} and he is ${catAge} years old`
+// reassign the value of the variable that references "null"
+catColor = 'orange '
+
+// print the value and its type
+
+// print a variable that causes a ReferenceError
+
+// alter the previous line to no longer cause a ReferenceError
+
+
+
+
